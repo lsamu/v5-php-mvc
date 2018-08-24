20993ae2d1e7ed634c1ee2f881485f9668534695
lauxinyi <lauxinyi@gmail.com>
2018-08-24 14:40:25 +0800

    master

--- demo/test.php
+++ demo/test.php
@@ -1,3 +1,9 @@
 <?php
-
+include ('../src/core/router.php');
+use V5\MVC;
+$router = new Router();
+//路由处理
+$router ->add("{controller}/{action}",['controller' => 'Home', 'action' => 'index']);
+//拦截器处理
+$router->dispatch("home/index");
 ?>
